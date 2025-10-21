7fc1f041a97910c3b0246e300264c319fdf33fc6
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 19:58:00 +0530

    Update fact_orders.sql

--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -5,21 +5,27 @@ select
     o.ORDERDATE as order_date,
     o.STATUS as order_status,
     o.CUSTOMERNUMBER as customer_id,
-    c.customer_name,
-    e.employee_id,  -- use alias from dim_employees
+    
+    c.CUSTOMERNAME as customer_name,
+    c.SALESREPEMPLOYEENUMBER as sales_rep_id,
+    
+    e.EMPLOYEENUMBER as employee_id,
     e.FIRSTNAME || ' ' || e.LASTNAME as employee_name,
-    e.office_id,    -- alias from dim_employees
-    p.product_id,
-    p.productName as product_name,
-    p.buyPrice,
+    e.OFFICECODE as office_id,
+    
+    p.PRODUCTCODE as product_id,
+    p.PRODUCTNAME as product_name,
+    p.BUYPRICE as buy_price,
+    
     od.QUANTITYORDERED as quantity,
     (od.QUANTITYORDERED * od.PRICEEACH) as total_amount
+
 from {{ ref('stg_orders') }} o
 join {{ ref('stg_orderdetails') }} od
     on o.ORDERNUMBER = od.ORDERNUMBER
 join {{ ref('dim_customers') }} c
     on o.CUSTOMERNUMBER = c.customer_id
-join {{ ref('dim_employees') }} e
-    on c.employee_id = e.employee_id  -- match alias
+left join {{ ref('dim_employees') }} e
+    on c.SALESREPEMPLOYEENUMBER = e.EMPLOYEENUMBER
 join {{ ref('dim_products') }} p
-    on od.PRODUCTCODE = p.product_id
+    on od.PRODUCTCODE = p.PRODUCTCODE
