eadc845a311f4ab264f5e5378b09d6b06b32b52c
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 20:07:23 +0530

    Update dim_customers.sql

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,18 +1,5 @@
 {{ config(materialized='table') }}
 
-select
-    customer_sk,
-    customer_id,
-    customer_name,
-    contactFirstName,
-    contactLastName,
-    phone,
-    addressLine1,
-    addressLine2,
-    city,
-    state,
-    postal_code,
-    country,
-    creditLimit,
-    employee_id
+select *
 from {{ ref('stg_customers') }}
+
