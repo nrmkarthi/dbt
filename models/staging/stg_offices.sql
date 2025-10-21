251df7c1cb82da809dfdddede0e2198f1d27bf6c
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 20:04:48 +0530

    Update stg_offices.sql

--- models/staging/stg_offices.sql
+++ models/staging/stg_offices.sql
@@ -1,17 +1,18 @@
 {{ config(materialized='view') }}
 
 select
-    {{ dbt_utils.generate_surrogate_key(['officeCode']) }} as office_sk,
-    officeCode as office_id,
-    city,
-    phone,
-    addressLine1,
-    addressLine2,
-    state,
-    country,
-    postalCode as postal_code,
-    territory
+    OFFICECODE as office_id,
+    CITY,
+    PHONE,
+    ADDRESSLINE1 as address_line1,
+    ADDRESSLINE2 as address_line2,
+    STATE,
+    COUNTRY,
+    POSTALCODE as postal_code,
+    TERRITORY
 from {{ source('sales', 'offices') }}
+
+
 
 
 
