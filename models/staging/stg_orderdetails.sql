726263d70a96899c5afea35c11d2474b7567994c
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:56:35 +0530

    Update stg_orderdetails.sql

--- models/staging/stg_orderdetails.sql
+++ models/staging/stg_orderdetails.sql
@@ -1,6 +1,9 @@
+{{ config(materialized='view') }}
+
 select
+    {{ dbt_utils.generate_surrogate_key(['orderNumber','productCode']) }} as orderdetail_sk,
     orderNumber as order_id,
     productCode as product_id,
     quantityOrdered as quantity,
     priceEach
-from {{ source('classicmodels','orderdetails') }}
+from {{ source('sales', 'orderdetails') }}
