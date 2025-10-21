d0b1ceab2abeac8a1def33ee69ee27b774ab8b44
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:59:03 +0530

    Update dim_products.sql

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,13 +1,15 @@
+{{ config(materialized='table') }}
+
 select
-    {{ dbt_utils.generate_surrogate_key(['p.productCode']) }} as product_sk,
-    p.productCode as product_id,
-    p.productName as product_name,
+    {{ dbt_utils.generate_surrogate_key(['p.product_id']) }} as product_sk,
+    p.product_id,
+    p.productName,
     p.productLine,
-    pl.product_line_description,  -- updated to match staging view
+    pl.product_line_description,
     p.productScale,
     p.productVendor,
     p.buyPrice,
     p.MSRP
 from {{ ref('stg_products') }} p
 left join {{ ref('stg_productlines') }} pl
-  on p.productLine = pl.product_line  -- also use staged column name
+  on p.productLine = pl.productLine
