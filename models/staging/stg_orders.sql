c69a2230f21c708a6f9268d097ad53c0722f15d3
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:56:10 +0530

    Update stg_orders.sql

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,9 +1,17 @@
+{{ config(materialized='view') }}
+
 select
+    {{ dbt_utils.generate_surrogate_key(['orderNumber']) }} as order_sk,
     orderNumber as order_id,
     orderDate,
+    requiredDate,
+    shippedDate,
     status,
+    comments,
     customerNumber as customer_id
-from {{ source('classicmodels','orders') }}
+from {{ source('sales', 'orders') }}
+
+
 
 
 
