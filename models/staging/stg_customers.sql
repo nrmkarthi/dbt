19d1c9d1f79ca0f8dfa3d0c2bee6c850c9241307
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:54:56 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,21 @@
-select *
-from {{ source('classicmodels','customers') }}
+{{ config(materialized='view') }}
+
+select
+    {{ dbt_utils.generate_surrogate_key(['customerNumber']) }} as customer_sk,
+    customerNumber as customer_id,
+    customerName as customer_name,
+    contactFirstName,
+    contactLastName,
+    phone,
+    addressLine1,
+    addressLine2,
+    city,
+    state,
+    postalCode as postal_code,
+    country,
+    creditLimit,
+    salesRepEmployeeNumber as employee_id
+from {{ source('sales', 'customers') }}
+
+
 
