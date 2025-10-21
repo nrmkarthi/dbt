9f4bbeb78653f678a58abfda876de608017b2e16
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:59:35 +0530

    Update fact_orders.sql

--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -1,15 +1,16 @@
+{{ config(materialized='table') }}
+
 select
-    {{ dbt_utils.generate_surrogate_key(['o.order_id','od.product_id']) }} as order_sk,
     o.order_id,
-    o.orderDate as order_date,
+    o.orderDate,
     o.status,
-    o.customer_id,
+    c.customer_id,
     c.customer_name,
     e.employee_id,
     e.firstName || ' ' || e.lastName as employee_name,
     e.office_id,
     p.product_id,
-    p.product_name,
+    p.productName,
     p.buyPrice,
     od.quantity,
     (od.quantity * od.priceEach) as total_amount
@@ -19,6 +20,6 @@ join {{ ref('stg_orderdetails') }} od
 join {{ ref('dim_customers') }} c
     on o.customer_id = c.customer_id
 join {{ ref('dim_employees') }} e
-    on c.employee_id = e.employee_id        -- use alias here
+    on c.employee_id = e.employee_id
 join {{ ref('dim_products') }} p
     on od.product_id = p.product_id
