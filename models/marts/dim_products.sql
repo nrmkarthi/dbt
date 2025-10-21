da137edea130dca3923fdb96f569ce96183f98cb
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:35:06 +0530

    Update dim_products.sql

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -3,12 +3,11 @@ select
     p.productCode as product_id,
     p.productName as product_name,
     p.productLine,
-    pl.textDescription as product_line_description,
+    pl.product_line_description,  -- updated to match staging view
     p.productScale,
     p.productVendor,
     p.buyPrice,
     p.MSRP
 from {{ ref('stg_products') }} p
 left join {{ ref('stg_productlines') }} pl
-  on p.productLine = pl.productLine
-
+  on p.productLine = pl.product_line  -- also use staged column name
