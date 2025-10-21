eed64c157f21d957990a7a2a1e8ced2969637549
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 20:08:31 +0530

    Update dim_products.sql

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,15 +1,16 @@
 {{ config(materialized='table') }}
 
 select
-    {{ dbt_utils.generate_surrogate_key(['p.product_id']) }} as product_sk,
     p.product_id,
-    p.productName,
-    p.productLine,
-    pl.product_line_description,
-    p.productScale,
-    p.productVendor,
-    p.buyPrice,
+    p.product_name,
+    p.product_line,
+    pl.text_description as product_line_description,
+    p.product_scale,
+    p.product_vendor,
+    p.buy_price,
     p.MSRP
 from {{ ref('stg_products') }} p
 left join {{ ref('stg_productlines') }} pl
-  on p.productLine = pl.productLine
+    on p.product_line = pl.product_line
+
+
