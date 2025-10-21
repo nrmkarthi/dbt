e89f236e55d0a31486075b7ba1cad307cec647ec
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:22:27 +0530

    Create stg_orderdetails.sql

---
+++ models/staging/stg_orderdetails.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+select
+    orderNumber as order_id,
+    productCode as product_id,
+    quantityOrdered as quantity,
+    priceEach
+from {{ source('classicmodels','orderdetails') }}
