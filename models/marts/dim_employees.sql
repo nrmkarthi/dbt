5e87f617c9e71f1c338a35017122d2bfb86efa80
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 20:07:46 +0530

    Update dim_employees.sql

--- models/marts/dim_employees.sql
+++ models/marts/dim_employees.sql
@@ -1,15 +1,9 @@
 {{ config(materialized='table') }}
 
-select
-    EMPLOYEENUMBER as employee_id,
-    FIRSTNAME,
-    LASTNAME,
-    EXTENSION,
-    EMAIL,
-    OFFICECODE as office_id,     -- use this alias
-    REPORTSTO,
-    JOBTITLE
-from {{ source('sales', 'employees') }}
+select *
+from {{ ref('stg_employees') }}
+
+
 
 
 
