14ea3b5972d6a4f0a112c7970069ac79bb77cf71
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:55:23 +0530

    Update stg_employees.sql

--- models/staging/stg_employees.sql
+++ models/staging/stg_employees.sql
@@ -1,3 +1,15 @@
-select *
+{{ config(materialized='view') }}
 
-from {{ source('classicmodels','employees') }}
+select
+    {{ dbt_utils.generate_surrogate_key(['employeeNumber']) }} as employee_sk,
+    employeeNumber as employee_id,
+    firstName,
+    lastName,
+    extension,
+    email,
+    officeCode as office_id,
+    reportsTo,
+    jobTitle
+from {{ source('sales', 'employees') }}
+
+
