b33e4cf0b2a92207828b90ea32368737dd657f24
nrmkarthi <nrmkarthi@gmail.com>
2025-10-21 18:57:13 +0530

    Update stg_products.sql

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,3 +1,16 @@
-select *
-from {{ source('classicmodels','products') }}
+{{ config(materialized='view') }}
+
+select
+    {{ dbt_utils.generate_surrogate_key(['productCode']) }} as product_sk,
+    productCode as product_id,
+    productName,
+    productLine,
+    productScale,
+    productVendor,
+    productDescription,
+    quantityInStock,
+    buyPrice,
+    MSRP
+from {{ source('sales', 'products') }}
+
 
